3853b8527f651907881004ff824290975dbd56d8
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-11 15:22:46 -0300

    b

---
+++ WinGrupo3B/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinGrupo3B
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCrearTransporte = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCrearTransporte
+            // 
+            this.btnCrearTransporte.Location = new System.Drawing.Point(203, 128);
+            this.btnCrearTransporte.Name = "btnCrearTransporte";
+            this.btnCrearTransporte.Size = new System.Drawing.Size(147, 23);
+            this.btnCrearTransporte.TabIndex = 0;
+            this.btnCrearTransporte.Text = "Crear Transporte";
+            this.btnCrearTransporte.UseVisualStyleBackColor = true;
+            this.btnCrearTransporte.Click += new System.EventHandler(this.btnCrearTransporte_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 303);
+            this.Controls.Add(this.btnCrearTransporte);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCrearTransporte;
+    }
+}
+
---
+++ WinGrupo3B/Form1.cs
@@ -0,0 +1,28 @@
+﻿using LibEntidades.Models.BASE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinGrupo3B
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCrearTransporte_Click(object sender, EventArgs e)
+        {
+            Avion avion_comercial = new Avion("Avión comercial", "Boeing 787", 2019, 140, "Turbina", "Transporte pasajeros", "Julian Cook", "Flybondi");
+
+            MessageBox.Show("Nombre: " + avion_comercial.Nombre + "\nModelo: " + avion_comercial.Modelo + "\nAño: " + avion_comercial.Anio + "\nCapacidad de pasajeros: " + avion_comercial.CapPasajeros + "\nModo de vuelo: " + avion_comercial.ModoVuelo + "\nFunción: " + avion_comercial.Funcionalidad + "\nPropietario: " + avion_comercial.Propietario + "\nAerolínea: " + avion_comercial.Aerolinea);
+        }
+    }
+}
